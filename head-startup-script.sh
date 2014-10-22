ce33034ee14b0d5e7472bc46bfecd3ceb71141dc
Keith Rochford <rochfordk@wifi-visitor228.heanet.ie>
2014-10-22 10:25:27 +0100

    fixed host key checking

--- head-startup-script.sh
+++ head-startup-script.sh
@@ -1,14 +1,14 @@
 #head startup script
 #!/bin/bash
-sudo usermod -d /home/shared -m mpiuser
-sudo chmod 740 /home/shared
-sudo runuser -l mpiuser -c 'ssh-keygen -t rsa -f /home/shared/.ssh/id_rsa -P ""'
-sudo runuser -l mpiuser -c 'cat /home/shared/.ssh/id_rsa.pub >> /home/shared/.ssh/authorized_keys'
-sudo runuser -l mpiuser -c 'cat /home/shared/.ssh/id_rsa.pub >> /home/shared/.ssh/authorized_keys2'
-sudo runuser -l mpiuser -c 'echo "StrictHostKeyChecking no" >> /etc/ssh/sshd_config'
-sudo runuser -l mpiuser -c 'chmod go-w ~'
-sudo runuser -l mpiuser -c 'chmod 700 /home/shared/.ssh'
-sudo runuser -l mpiuser -c 'chmod 644 /home/shared/.ssh/authorized_*'
-sudo echo '/home/shared *(rw,async)' | sudo tee -a /etc/exports
-sudo /bin/systemctl start nfs.service
+usermod -d /home/shared -m mpiuser
+chmod 740 /home/shared
+runuser -l mpiuser -c 'ssh-keygen -t rsa -f /home/shared/.ssh/id_rsa -P ""'
+runuser -l mpiuser -c 'cat /home/shared/.ssh/id_rsa.pub >> /home/shared/.ssh/authorized_keys'
+#runuser -l mpiuser -c 'cat /home/shared/.ssh/id_rsa.pub >> /home/shared/.ssh/authorized_keys2'
+#runuser -l mpiuser -c 'chmod go-w ~'
+runuser -l mpiuser -c 'chmod 700 /home/shared/.ssh'
+runuser -l mpiuser -c 'chmod 644 /home/shared/.ssh/authorized_*'
+echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo '/home/shared *(rw,async)' | tee -a /etc/exports
+/bin/systemctl start nfs.service
 
